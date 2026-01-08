915dd0c812ca74d853b395d664ba42fce07279ca
shironeko1337 <shironeko1052@gmail.com>
2026-01-07 19:15:10 -0800

    Update vite.config.ts for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  //base: '/<YOUR GITHUB REPO NAME e.g. heroui-vite-boilerplate IF USING GITHUB PAGES>/'
 });
